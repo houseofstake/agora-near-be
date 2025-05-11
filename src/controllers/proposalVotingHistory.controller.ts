5855b31b0237d7898e6c73c38b0af56c3d10a696
Joshua Carnide <joshuacarnide@gmail.com>
2025-05-10 20:53:07 -0700

    Only return needed data

--- src/controllers/proposalVotingHistory.controller.ts
+++ src/controllers/proposalVotingHistory.controller.ts
@@ -18,18 +18,30 @@ export class ProposalVotingHistoryController {
         skip: (pageNumber - 1) * pageSize,
         take: pageSize,
         orderBy: {
-          votingPower: 'desc',
+          votingPower: "desc",
         },
-      })
+      });
 
       const count = await prisma.proposalVotingHistory.count({
         where: { proposalId },
-      })
+      });
 
-      res.status(200).json({ records, count });
+      const voteRecords = records.map((record) => {
+        const { votingPower, voterId, voteOption } = record;
+
+        return {
+          accountId: voterId,
+          votingPower: votingPower.toString(),
+          voteOption: voteOption.toString(),
+        };
+      });
+
+      res.status(200).json({ votes: voteRecords, count });
     } catch (error) {
       console.error("Error fetching proposal voting history:", error);
-      res.status(500).json({ error: "Failed to fetch proposal voting history" });
+      res
+        .status(500)
+        .json({ error: "Failed to fetch proposal voting history" });
     }
   };
 }
