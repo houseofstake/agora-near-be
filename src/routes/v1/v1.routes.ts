bcb9442e85eedd675c1e485d78bb997b20d26049
Atomauro <mauro.henaog@gmail.com>
2026-03-13 13:51:30 -0500

    feat(web2): rename read:forum scope to read:data

--- src/routes/v1/v1.routes.ts
+++ src/routes/v1/v1.routes.ts
@@ -7,11 +7,11 @@ const router = Router();
 // /v1/me -> Returns profile info. Requires no special scopes, just a valid key.
 router.get("/me", apiKeyAuth() as unknown as RequestHandler, getAgentProfile as unknown as RequestHandler);
 
-// /v1/proposals -> Requires 'read:forum' scope
-router.get("/proposals", apiKeyAuth(["read:forum"]) as unknown as RequestHandler, getProposals as unknown as RequestHandler);
+// /v1/proposals -> Requires 'read:data' scope
+router.get("/proposals", apiKeyAuth(["read:data"]) as unknown as RequestHandler, getProposals as unknown as RequestHandler);
 
-// /v1/delegates -> Requires 'read:forum' scope
-router.get("/delegates", apiKeyAuth(["read:forum"]) as unknown as RequestHandler, getDelegates as unknown as RequestHandler);
+// /v1/delegates -> Requires 'read:data' scope
+router.get("/delegates", apiKeyAuth(["read:data"]) as unknown as RequestHandler, getDelegates as unknown as RequestHandler);
 
 // /v1/vote -> Requires 'write:vote' scope
 router.post("/vote", apiKeyAuth(["write:vote"]) as unknown as RequestHandler, castProxyVote as unknown as RequestHandler);
